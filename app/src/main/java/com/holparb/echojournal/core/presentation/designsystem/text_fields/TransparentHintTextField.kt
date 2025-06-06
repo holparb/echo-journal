c223771e4eda84b0b0faf88bb0ce6e8947219a16
Balazs Holpar <balazs02.holpar@gmail.com>
2025-06-06 11:38:04 +0200

    Update TransparentHintTextField default style

--- app/src/main/java/com/holparb/echojournal/core/presentation/designsystem/text_fields/TransparentHintTextField.kt
+++ app/src/main/java/com/holparb/echojournal/core/presentation/designsystem/text_fields/TransparentHintTextField.kt
@@ -21,8 +21,8 @@ fun TransparentHintTextField(
     modifier: Modifier = Modifier,
     hintText: String? = null,
     hintColor: Color = MaterialTheme.colorScheme.outlineVariant,
-    textStyle: TextStyle = MaterialTheme.typography.headlineLarge.copy(
-        color = MaterialTheme.colorScheme.onSurface
+    textStyle: TextStyle = MaterialTheme.typography.bodyMedium.copy(
+        color = MaterialTheme.colorScheme.outlineVariant
     ),
     maxLines: Int = Int.MAX_VALUE,
     singleLine: Boolean = false,
